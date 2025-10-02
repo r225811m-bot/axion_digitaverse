080e50a446c762da9b41e730d48f0716280571da
r225811m <r225811m@students.msu.ac.zw>
2025-10-02 15:15:50 +0000

    update

--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -1,5 +1,14 @@
 import { createContext, useContext, useState } from "react";
 
+export interface User {
+  address: string;
+  username: string;
+  balance: number;
+  profilePic: string;
+  isMiner: boolean;
+  token: string;
+}
+
 const UserContext = createContext<any>(null);
 
 export function UserProvider({ children }: any) {
